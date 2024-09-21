1834b2f9e2195b5bbb47c35cfed9f55b85d42dee
DSalado97 <danielsalado@protonmail.com>
2024-09-21 16:12:17 +0200

    Diseño de la página e intento de conexión. Falla :(

--- ng-chat-socket/src/app/components/chat/chat.component.ts
+++ ng-chat-socket/src/app/components/chat/chat.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../../services/chat.service';
+import { ChatMessage } from '../../models/chat-message';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
-  standalone: true,
-  imports: [],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss'
+  styleUrls: ['./chat.component.scss']
+  /*
+  standalone: true,  // Esto indica que es un componente standalone
+  imports: [FormsModule]  // Importamos FormsModule directamente
+*/
 })
-export class ChatComponent {
+
+export class ChatComponent implements OnInit {
+
+  messageInput: string = '';
+  userId: string = "";
+  messageList: any[] = [];
+  constructor(private chatService:ChatService,
+    private route: ActivatedRoute
+  ) {}
+  
+  
+  ngOnInit(): void {
+    this.userId = this.route.snapshot.params["userId"];
+    this.chatService.joinRoom("ABC");
+  }
+
+  sendMessage(){
+    const chatMessage = {
+      message: this.messageInput,
+      user: this.userId
+    }as ChatMessage
+    this.chatService.sendMessage("ABC", chatMessage);
+    this.messageInput = '';
+  }
+ 
 
 }
--- ng-chat-socket/src/app/services/chat.service.ts
+++ ng-chat-socket/src/app/services/chat.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import {Stomp} from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+import { ChatMessage } from '../models/chat-message';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor() { }
+  private stompClient: any
+
+  constructor() {
+    this.initConnectionSocket()
+   }
+
+  // Va a iniciar la conexión con el Socket
+  initConnectionSocket(){
+    const url ='http://localhost:3000/chat-socket';
+    const socket = new SockJS(url);
+    this.stompClient = Stomp.over(socket);
+  }
+
+  // No todos los usuarios pueden conectarse al socket.
+  // Solo los asociados al roomId
+  joinRoom(roomId: string) {
+    this.stompClient.connect({}, () => {
+      this.stompClient.suscribe(`/topic/${roomId}`, (messages: any) =>{
+        const messageContent = JSON.parse(messages.body);
+        console.log(messageContent)
+      })
+    })
+  }
+
+  sendMessage(roomId: string, chatMessage: ChatMessage){
+    this.stompClient.send(`/app/chat/${roomId}`, {}, JSON.stringify(chatMessage))
+  }
 }
